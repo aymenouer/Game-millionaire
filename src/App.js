dce5588d547a4abb06e5f54d266cf9c5ab52fe17
Aymen <aymen.ouerghui@esprit.tn>
2021-06-12 15:26:55 +0200

    premiere interface

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+     <div className="main">
+     main
+     </div>
+     <div className="pyramid">
+     pyramid
+     </div>
+    </div>
+  );
+}
+
+export default App;
